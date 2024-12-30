7b562b6c1fc54aa73f9af8c7bda108d79cb309fb
Gabriel Choong <gabrielchoonggeliang@gmail.com>
2024-12-30 20:01:53 +0800

    single page website

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    // output: "export",
+    // trailingSlash: true,
+    // basePath: "/gabrielchoong.github.io"
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,94 @@
+import "../styles.css";
+import "../local-styles.css";
+
 import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    return (
+        <div className={"flex-container"}>
+            <Head>
+                <meta charSet="utf-8"/>
+                <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0"/>
+            </Head>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <header>
+                <section className={"custom-container"}>
+                    <div className={"page-left"}>
+                        <h1>Hype-less AI</h1>
+                        <p>Just AI talk</p>
+                        <p><em>minus the jargon</em></p>
+                    </div>
+
+                    <div className={"page-right"}>
+                        <h2><strong>Projects</strong></h2>
+                        <a href="https://github.com/gabrielchoong" target="_blank">
+                            <Image src={"/assets/images/github-mark/github-mark-white.svg"} alt={"GitHub Logo"}
+                                   width={30}
+                                   height={30}/></a>
+
+                        <a href="https://www.kaggle.com/gabrielchoong" target="_blank">
+                            <Image src={"/assets/images/kaggle/kaggle-svgrepo-com.svg"} alt={"Kaggle Logo"}
+                                   width={30}
+                                   height={30}/></a>
+
+                        <a href="https://huggingface.co/Gabriel001" target="_blank">
+                            <Image src={"/assets/images/hugging-face/hf-logo.svg"} alt={"Hugging Face Logo"}
+                                   width={30}
+                                   height={30}/></a>
+                    </div>
+                </section>
+            </header>
+
+            <main>
+                <section className={"custom-container"}>
+                    <div className={"page-left"}>
+                        <div className={"blog-posts"}>
+                            <div className={"post"}>
+                                <h2>Site updating - 2024-10-26</h2>
+                                <p>Hey just so for the curious viewer, I&#39;m currently updating all of my
+                                    code, and
+                                    I&#39;ve taken down all my blogs that absolutely no one read. But, don&#39;t
+                                    panic, in a
+                                    couple of days they should be up and running. I&#39;m in the process of setting
+                                    things up so it&#39;s semi-automated. In the mean time, enjoy an image of a
+                                    cat from an old <a
+                                        href={"https://cat-chitchat.pictures-of-cats.org/p/the-author-of.html"}
+                                        target={"_blank"}>blog</a>.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={"page-right"}>
+                        <h2><strong>Author</strong></h2>
+                        <p>Hi, my name is Gabriel, the author of this blog. I&#39;m a final year university student
+                            majoring
+                            in <strong>Artificial Intelligence</strong>. I&#39;m writing this blog to share my
+                            insights
+                            about this emerging field where <em>hype</em> is ruining everything. Let&#39;s dive into
+                            more
+                            intricate topics about AI without all the nonsensical arguments!</p>
+                        <br/>
+                        <p>If you&#39;re interested in what I do, consider checking my GitHub page linked above, or
+                            email me
+                            which are conveniently located at the top and bottom of this webpage. You could also
+                            stalk me
+                            on LinkedIn, see my competitions on Kaggle, or even figure out why Hugging Face is so
+                            popular
+                            among
+                            researchers!</p>
+                    </div>
+                </section>
+            </main>
+
+            <footer>
+                <p>Contact me via <a href="mailto:gabrielchoonggeliang@gmail.com"
+                                     target="_blank"><em>email</em></a> or <a
+                    href="https://linkedin.com/in/gabrielchoong" target="_blank"><em>LinkedIn</em></a></p>
+                <p>Copyright &copy; 2024 Gabriel Choong Ge Liang. All rights reserved.</p>
+            </footer>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+
+    );
 }
